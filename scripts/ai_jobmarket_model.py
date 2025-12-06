ed68fe1dc0b1ab15e9203ca3abb9333c43d1d327
faithma2 <156957901+faithma2@users.noreply.github.com>
2025-12-06 13:05:55 -0600

    Create ai_jobmarket_model.py

---
+++ scripts/ai_jobmarket_model.py
@@ -0,0 +1,45 @@
+# load integrated data
+df = pd.read_csv("data/processed/jobs_unified.csv")
+df.head()
+
+# train test split data
+X_train, X_test, y_train, y_test = train_test_split(df, df["Salary_USD"], test_size=0.2, random_state=15)
+
+# preprocessing
+
+numeric_features = ["Years_Experience"]
+categorical_features = ["Education_Required", "AI_Adoption_Level", "Automation_Risk", "Remote_Friendly", "Job_Growth_Projection"]
+
+categorical_transformer = Pipeline(
+    [
+        ("Imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder())
+    ]
+)
+numeric_transformer = Pipeline(
+    [
+        ("Imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler())
+    ]
+)
+
+column_transformer = ColumnTransformer(
+    [
+        ("categorical", categorical_transformer, categorical_features),
+        ("numeric", numeric_transformer, numeric_features)
+    ]
+)
+
+pipeline = Pipeline(
+    [
+        ("preprocessor", column_transformer),
+        ("regressor", RandomForestRegressor(n_estimators=5)) 
+    ]
+)
+
+# train model
+pipeline.fit(X_train,y_train)
+
+# test metrics
+y_pred = pipeline.predict(X_test)
+test_mape = mean_absolute_percentage_error(y_test,y_pred)
